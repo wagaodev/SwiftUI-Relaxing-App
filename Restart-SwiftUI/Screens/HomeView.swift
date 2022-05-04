dcc690844ef861d0e25c4657b3823b864565189b
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-04 00:54:15 -0300

    chore: remove capability to rotate the device

---
+++ Restart-SwiftUI/Screens/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart-SwiftUI
+//
+//  Created by WAGNER GOULART on 04/05/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
