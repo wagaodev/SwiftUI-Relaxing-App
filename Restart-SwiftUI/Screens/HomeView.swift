2b2eddc7093a42523b117edecd851dd5de1d23bb
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-04 01:08:46 -0300

    feat: we can navigate between onboarding and home screen

--- Restart-SwiftUI/Screens/HomeView.swift
+++ Restart-SwiftUI/Screens/HomeView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct HomeView: View {
+
+  // MARK: - Properties
+
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+
+  // MARK: - Body
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      VStack(spacing: 20) {
+        Text("Home")
+          .font(.largeTitle)
+
+        Button(action: {
+          isOnboardingViewActive = true
+        }) {
+          Text("Restart")
+        }
+      }
     }
 }
+
+// MARK: - Preview
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
--- Restart-SwiftUI/Screens/OnboardingView.swift
+++ Restart-SwiftUI/Screens/OnboardingView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct OnboardingView: View {
+
+  // MARK: - Properties
+
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+
+  // MARK: - Body
     var body: some View {
+      VStack(spacing: 20) {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+          .font(.largeTitle)
+
+        Button(action: {
+          isOnboardingViewActive = false
+        }) {
+          Text("Start")
+        }
+      }
     }
 }
+
+
+// MARK: - Preview
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
