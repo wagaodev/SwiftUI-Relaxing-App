138a30e2b53f5dbad4000467ea012a71a3f34d10
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-04 00:09:54 -0300

    chore: default structure

---
+++ Restart-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart-SwiftUI
+//
+//  Created by WAGNER GOULART on 04/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restart-SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart-SwiftUI
+//
+//  Created by WAGNER GOULART on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
