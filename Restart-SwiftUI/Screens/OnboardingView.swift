e734e2c4c696c4bfab6a10fc116de80725910b87
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-04 03:35:55 -0300

    feat: handling with gesture button

--- Restart-SwiftUI/Screens/OnboardingView.swift
+++ Restart-SwiftUI/Screens/OnboardingView.swift
@@ -12,6 +12,8 @@ struct OnboardingView: View {
   // MARK: - Properties
 
   @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+  @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+  @State private var buttonOffset: CGFloat = 0
 
   // MARK: - Body
   var body: some View {
@@ -84,15 +86,21 @@ struct OnboardingView: View {
             }
             .foregroundColor(.white)
             .frame(width: 80, height: 80, alignment: .center)
-            .onTapGesture {
-              isOnboardingViewActive = false
-            }
+            .offset(x: buttonOffset)
+            .gesture(
+              DragGesture()
+                .onChanged { gesture in
+                  if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                    buttonOffset = gesture.translation.width
+                  }
+                }
+            ) //MARK: - Button Gesture
             Spacer()
           }
         }
 
 
-        .frame(height: 80, alignment: .center)
+        .frame(width: buttonWidth, height: 80, alignment: .center)
         .padding()
       }
     }
