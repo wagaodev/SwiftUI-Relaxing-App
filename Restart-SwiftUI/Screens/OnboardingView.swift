ed0f12ca679a36108586c1b71806bdaab472ec37
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-04 23:31:36 -0300

    feat: create an animation with illustration

--- Restart-SwiftUI/Screens/OnboardingView.swift
+++ Restart-SwiftUI/Screens/OnboardingView.swift
@@ -15,6 +15,7 @@ struct OnboardingView: View {
   @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
   @State private var buttonOffset: CGFloat = 0
   @State private var isAnimating: Bool = false
+  @State private var imageOffset: CGSize = .zero
 
   // MARK: - Body
   var body: some View {
@@ -53,7 +54,19 @@ struct OnboardingView: View {
             .scaledToFit()
             .opacity(isAnimating ? 1 : 0)
             .animation(.easeOut(duration: 0.5), value: isAnimating)
-
+            .offset(x: imageOffset.width * 1.2, y: 0)
+            .gesture(
+              DragGesture()
+                .onChanged { gesture in
+                  if abs(imageOffset.width) <= 150 {
+                    imageOffset = gesture.translation
+                  }
+                }
+                .onEnded {_ in
+                  imageOffset = .zero
+                }
+            )
+            .animation(.easeOut(duration: 1), value: imageOffset)
         }
 
         Spacer()
